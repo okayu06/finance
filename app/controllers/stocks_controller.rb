2b134b9caf9a630479b5c658f39f3cad0b40d09d
Yu Hiraoka <magicsunset0805@gmail.com>
2018-09-10 23:39:08 +0000

    update ajax?

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -7,12 +7,16 @@ class StocksController < ApplicationController
         format.js { render partial: 'users/result'}
       end  
       else
-        flash[:danger] = "you have entered on incorrect symbol"
-        redirect_to my_portofolio_path
+        respond_to do |format|
+          flash.now[:danger] = "you have entered on incorrect symbol"
+          format.js { render partial:'users/result' }
+        end  
       end  
     else
-      flash[:danger] = "You have entered on empty search string"
-      redirect_to my_portofolio_path
+      respond_to do |format|
+          flash.now[:danger] = "you have entered an empty string" 
+          format.js { render partial:'users/result' }
+      end  
     end  
   end  
 end  
